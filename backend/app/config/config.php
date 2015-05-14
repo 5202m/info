b65dbef211e70a32d219a30ebba85845404c7b88
181 <181@cngaochuang.com>
2015-05-14 11:19:47 +0800

    #后台bootstrap总模板

--- backend/app/config/config.php
+++ backend/app/config/config.php
@@ -3,10 +3,10 @@
 return new \Phalcon\Config(array(
 	'database' => array(
 		'adapter'  => 'Mysql',
-		'host'     => 'localhost',
-		'username' => 'root',
-		'password' => '',
-		'name'     => 'test',
+		'host'     => 'db.hx9999.com',
+		'username' => 'hx9999.com',
+		'password' => 'hx9999.com',
+		'name'     => 'hx9999',
 	),
 	'application' => array(
 		'controllersDir' => __DIR__ . '/../../app/controllers/',
