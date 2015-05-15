8025879fbcd79de810d978b08f0e511968b1f75f
netkiller <netkiller@msn.com>
2015-05-15 15:27:34 +0800

    Issues #9 分类页渲染

--- frontend/app/controllers/CategoryController.php
+++ frontend/app/controllers/CategoryController.php
@@ -4,13 +4,30 @@ class CategoryController extends ControllerBase
 
 	public function indexAction()
 	{
-		echo 'asdfasf';
-		exit();
-        $category = Category::find(
-            "division_id = 3"
-        );
-        print_r($category);
+
+        
+        //print_r($category);
 	}
+	public function htmlAction($division_id){
+		$categorys = Category::find("division_id = $division_id");
+		foreach ($categorys as $category){
+			printf("%s, %s", $category->id, $category->name);
+		}
+	}
+	public function jsonAction($division_id){
+		$result = array();
+		$this->view->disable();
+		$categorys = Category::find("division_id = $division_id");
+		foreach ($categorys as $category){
+			$result[$category->id]=$category->name;
+		}
+		$response = new Phalcon\Http\Response();
+		$response->setHeader('Cache-Control', 'max-age=60');
+		$response->setContentType('application/json', 'utf-8');
+		$response->setContent(json_encode($result));
+		return $response;
+	}
+	
 	public function pageAction(){
 		 
 	}
