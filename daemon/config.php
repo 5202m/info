4b62f1d32412cad6bdbfec6d511af3bf4612d0f2
netkiller <netkiller@msn.com>
2015-05-28 11:09:22 +0800

    config.php

---
+++ daemon/config.php
@@ -0,0 +1,19 @@
+<?php
+array (
+		'export' => array (
+			'dbhost' => '127.0.0.1',
+			'dbport' => '3306',
+			'dbuser' => 'gwfx',
+			'dbpass' => 'gwfx',
+			'dbname' => 'whdata'
+		),
+		'import' => array (
+			'dbhost' => '192.168.6.1',
+			'dbport' => '3306',
+			'dbuser' => 'inf',
+			'dbpass' => 'inf',
+			'dbname' => 'inf'
+		)
+	);
+?>
+
