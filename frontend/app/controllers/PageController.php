323530facacb400862a5a97f0b97fe65b7ac20b7
netkiller <netkiller@msn.com>
2015-09-02 14:29:37 +0800

    优化输入

--- frontend/app/controllers/PageController.php
+++ frontend/app/controllers/PageController.php
@@ -19,12 +19,13 @@ class PageController extends ControllerBase
 		
     	if(empty($template_id) || empty($dir_name)){
     		$this->response->setStatusCode(404, 'Not Found');
-			return;
+			$this->response->setContent('Template ID Not Found');
+    		return($this->response);
     	}
 
-    	$template_file = sprintf("%s/template/%s/%s.phtml", $this->basedir, $dir_name, $template_id);
+    	$template_file = sprintf("%s/template/page/%s/%s.phtml", $this->basedir, $dir_name, $template_id);
     	$page_file = sprintf("%s/static/page/html/%s/%s.html", $this->basedir, $dir_name, $template_id);
-    	
+		
     	if(!is_file($template_file)){
     		 
     		$template = Template::findFirst(array(
@@ -42,22 +43,23 @@ class PageController extends ControllerBase
     			file_put_contents($template_file , $template->content);
     		}else{
     			$this->response->setStatusCode(404, 'Template Not Found');
-    			echo 'Template Not Found';
-    			return;
+				$this->response->setContent('Template Not Found');
+    			return($this->response);
     		}
     	}
 		
 		if(is_file($template_file)){
+			/*
 			if(is_file($page_file)){
 				
 			}else{
 				
 			}
-			
+			*/
 	    	$view = new \Phalcon\Mvc\View();
-	    	$view->setViewsDir($this->basedir.'/template');
+	    	$view->setViewsDir($this->basedir.'/template/page');
 	    	$view->setRenderLevel(Phalcon\Mvc\View::LEVEL_LAYOUT);
-	    	//$view->setVar('article',$article);
+	    	//$view->setVars($data);
 			//$view->cache(array('key' => 'my-key', 'lifetime' => 86400));
 			$view->sss = "aaa";
 	    	$view->start();
@@ -71,21 +73,21 @@ class PageController extends ControllerBase
 	    	}
 			
 			if($content){
-				//file_put_contents($page_file, $content);
+				file_put_contents($page_file, $content);
 			}			
 			
-			//$expireDate = new DateTime();
-			//$expireDate->modify('+1 minutes');
-			//$this->response->setExpires($expireDate);
-			$this->response->setHeader('ETag', $eTag = crc32($content));			
+			$expireDate = new DateTime();
+			$expireDate->modify('+1 minutes');
+			$this->response->setExpires($expireDate);
 	    	$this->response->setHeader('Cache-Control', 'max-age=60');
-			
-			//$this->response->setContent();
-			echo ($content);
-	    	//print(file_get_contents($template_file)); 
+			$this->response->setHeader('ETag', $eTag = crc32($content));
+			$this->response->setContent($content);
+
+			return($this->response);
 		}else{
 			$this->response->setStatusCode(404, 'Article Not Found');
-			echo 'Article Not Found';
+			$this->response->setContent('Article Not Found');
+			return($this->response);
 		} 	
     	
     }
