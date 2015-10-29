5385c26646df06b7913a1fc62cfa39472f85c3df
liuxiang <168@cngaochuang.com>
2015-10-29 11:00:55 +0800

    添加任务总数

--- sender/app/models/Task.php
+++ sender/app/models/Task.php
@@ -3,7 +3,7 @@
 class Task extends \Phalcon\Mvc\Model
 {
     public function initialize(){
-        Task::skipAttributes(array('mtime','gateway','ctime'));
+        Task::skipAttributes(array('mtime','gateway','ctime','total'));
         $this->belongsTo("group_id", "Group", "id");
         $this->belongsTo("template_id", "Template", "id");
         $this->belongsTo("message_id", "Message", "id");
